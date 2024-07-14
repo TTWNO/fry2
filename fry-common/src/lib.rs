f5460dd735f302eea6f1c44b6843bc3c2121aed8
Tait Hoyem <tait@tait.tech>
2024-07-13 21:28:53 -0600

    Add some docs

--- fry-common/src/lib.rs
+++ fry-common/src/lib.rs
@@ -1,4 +1,11 @@
+//! `fry-common`
+//!
+//! Basic data structures and algorithms for text-to-speech (TTS) processing.
+//! Based on:
+//! - [Festival](https://github.com/festvox/festival)
+//! - [flite](https://github.com/festvox/flite)
+//! - [Edinburgh Speech Tools (EST)](https://github.com/festvox/speech_tools)
 #![no_std]
-#[forbid(clippy::all, clippy::pedantic, missing_docs)]
+#![forbid(clippy::all, clippy::pedantic, missing_docs)]
 
 pub mod regex;
