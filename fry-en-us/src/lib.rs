9986627a141a8a2eddc179df3c5d9d0f8ff5c74a
Tait Hoyem <tait@tait.tech>
2024-08-23 20:47:38 -0600

    Add new lints

--- fry-en-us/src/lib.rs
+++ fry-en-us/src/lib.rs
@@ -1,6 +1,8 @@
 //! Translation of the algorithms from the `lang/usenglish/` directory in the flite text-to-speech
 //! engine.
 #![no_std]
+#![deny(clippy::all, clippy::pedantic, clippy::cargo, rustdoc::all)]
+#![forbid(unsafe_code)]
 
 pub mod aswd;
 pub mod error;
