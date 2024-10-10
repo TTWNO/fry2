75669d0bc2d3130fb15f7f24564a7708c7a551ed
Tait Hoyem <tait@tait.tech>
2024-10-10 08:47:48 -0600

    add relation variant to path

--- fry-common/src/path.rs
+++ fry-common/src/path.rs
@@ -1,35 +1,64 @@
 //! Path components for representing relations between tree items.
 
-use strum::EnumString;
-
 /// A single path component (where to go to next) in a tree.
 ///
 /// <http://www.festvox.org/docs/manual-2.4.0/festival_14.html#Features>
-#[derive(PartialEq, Eq, EnumString)]
+#[derive(PartialEq, Eq, Debug)]
 #[allow(missing_docs)]
-pub enum Path {
-    #[strum(serialize="n")]
+pub enum Path<'a> {
     Next,
-    #[strum(serialize="p")]
     Previous,
-    #[strum(serialize="first")]
     First,
-    #[strum(serialize="last")]
     Last,
-    #[strum(serialize="parent")]
     Parent,
-    #[strum(serialize="nn")]
     NextNext,
-    #[strum(serialize="pp")]
     PreviousPrevious,
-    #[strum(serialize="daughter",serialize="daughter1")]
     Daughter,
-    #[strum(serialize="daughter2")]
     SecondDaughter,
-    #[strum(serialize="daughtern")]
     LastDaughter,
-    #[strum(serialize="R")]
-    Relation,
+    Relation(&'a str),
+}
+
+impl<'a> TryFrom<&'a str> for Path<'a> {
+    type Error = PathError<'a>;
+    fn try_from(s: &'a str) -> Result<Path<'a>, Self::Error> {
+        use Path::*;
+        match s {
+            "n" => return Ok(Next),
+            "p" => return Ok(Previous),
+            "first" => return Ok(First),
+            "last" => return Ok(Last),
+            "parent" => return Ok(Parent),
+            "nn" => return Ok(NextNext),
+            "pp" => return Ok(PreviousPrevious),
+            "daughter" | "daughter1" => return Ok(Daughter),
+            "daughter2" => return Ok(SecondDaughter),
+            "daughtern" => return Ok(LastDaughter),
+            "" => return Err(PathError::MissingFeature),
+            _ => {},
+        };
+        let mut split = s.split(":");
+        let Some(r_colon) = split.next() else {
+            return Err(PathError::MissingFeature);
+        };
+        if r_colon != "R" {
+            return Err(PathError::InvalidFeature(r_colon));
+        }
+        let Some(rel) = split.next() else {
+            return Err(PathError::MissingRelation);
+        };
+        Ok(Relation(rel))
+    }
+}
+
+/// An error serializing the path.
+pub enum PathError<'a> {
+    /// Missing a feature; blank string
+    MissingFeature,
+    /// Invalid feature (not one of the accepted features)
+    InvalidFeature(&'a str),
+    /// Missing relation; blank string after "R:"
+    MissingRelation,
 }
 
 
