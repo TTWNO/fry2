e1c446054be86e685f0ada62a1b7b848482565fe
Tait Hoyem <tait@tait.tech>
2024-11-09 11:00:03 -0700

    Use fully-features Val types; abandon attempt to not let refcells explode

--- fry-common/src/val.rs
+++ fry-common/src/val.rs
@@ -1,18 +1,47 @@
 //! CST Value based on `inclue/cst_val.h` in _Flite_
 
 use crate::{
-    error::ValueError, CartTree, Feature, Item, MaybeStrong, Phoneset, Relation, Strong, Utterance,
+    error::ValueError, Feature, Phoneset, Relation, Strong, Utterance,
 };
 use alloc::{
-    rc::{Rc, Weak},
     vec::Vec,
+		boxed::Box,
 };
 use core::str::FromStr;
 use indextree::NodeId;
 use strum::{Display, EnumDiscriminants};
 
+/// Either:
+///
+/// - A [`ValueAtom`] in a list, or
+/// - A single [`ValueAtom`] value
+///
+/// [Named `Cons` and `Atom` respectively because of Lisp naming schemes](https://en.wikipedia.org/wiki/Lisp_(programming_language)#Lists)
+/// 
+/// This type implements [`core::ops::Deref`] for the inner [`ValueAtom`] structure of either the `Cons` or `Atom` variants.
+#[derive(derive_more::From)]
+pub enum CstValue<'a> {
+	/// List variant, first item is data, second is the next item
+	Cons((ValueAtom<'a>, Box<CstValue<'a>>)),
+	/// Single value
+	Atom(ValueAtom<'a>),
+}
+impl<'a> core::ops::Deref for CstValue<'a> {
+	type Target = ValueAtom<'a>;
+	fn deref(&self) -> &ValueAtom<'a> {
+		match self {
+			CstValue::Cons((ref atom, _)) => atom,
+			CstValue::Atom(ref atom) => atom,
+		}
+	}
+}
+
+/// Value atom (AKA a `Strong<Value>`)
+#[derive(derive_more::From, derive_more::Deref)]
+pub struct ValueAtom<'a>(#[deref] Strong<Value<'a>>);
+
 #[repr(u8)]
-#[derive(Debug, Clone, PartialEq, EnumDiscriminants)]
+#[derive(Debug, Clone, PartialEq, EnumDiscriminants, derive_more::From)]
 #[strum_discriminants(derive(Display))]
 /// A generic value, which could be a `String`, `Int` (16 bits), or `Float` (32 bits)
 pub enum Value<'a> {
@@ -24,60 +53,60 @@ pub enum Value<'a> {
     /// A float
     Float(f32),
     /// Utterance
-    Utterance(MaybeStrong<Utterance<'a>>) = 7,
-    /// TODO: wave
-    Wave(()) = 9,
-    /// TODO: track
-    Track(()) = 11,
-    /// TODO: `LPCres`
-    Lpcres(()) = 13,
-    /// TODO: `UttFunc`
-    UttFunc(()) = 15,
-    /// TODO: ffunc
-    FFunc(()) = 17,
+    Utterance(Utterance<'a>) = 7,
+    ///// TODO: wave
+    //Wave(()) = 9,
+    ///// TODO: track
+    //Track(((),)) = 11,
+    ///// TODO: `LPCres`
+    //Lpcres(((),(),)) = 13,
+    ///// TODO: `UttFunc`
+    //UttFunc(((),(),(),)) = 15,
+    ///// TODO: ffunc
+    //FFunc(((),(),(),(),())) = 17,
     /// TODO: relation
-    Relation(MaybeStrong<Relation<'a>>) = 19,
+    Relation(Relation<'a>) = 19,
     /// TODO: item; encoded as a `NodeId` so that it can grab the Item from the arena
     Item(NodeId) = 21,
-    /// TODO: cart tree
+    ///// TODO: cart tree
     //Cart(&'a CartTree<'a, 1, 1>) = 23,
     /// TODO: phoneset
-    Phoneset(MaybeStrong<Phoneset<'a>>) = 25,
-    /// TODO: lexicon
-    Lexicon(()) = 27,
-    /// TODO: durstats
+    Phoneset(Phoneset<'a>) = 25,
+    //// TODO: lexicon
+    //Lexicon(()) = 27,
+    //// TODO: durstats
     //DurStats(()) = 29,
-    /// TODO: diphonedb
-    DiphoneDb(()) = 31,
-    /// TODO: clunitdb
-    ClunitDb(()) = 33,
-    /// TODO: `vit_cand`
-    VitCand(()) = 35,
-    /// TODO: `sts_list`
-    StsList(()) = 37,
-    /// TODO: userdata
-    UserData(()) = 41,
-    /// TODO: itemfunc
-    ItemFunc(()) = 43,
-    /// TODO: features
-    Features(MaybeStrong<Vec<Feature<'a>>>) = 45,
-    /// TODO: breakfunc
-    BreakFunc(()) = 47,
-    /// TODO: `cg_db`
-    ClustergenDb(()) = 49,
-    /// TODO: voice
-    Voice(()) = 51,
-    /// TODO: `audio_streaming_info`
-    AudioStreamingInfo(()) = 53,
+    //// TODO: diphonedb
+    //DiphoneDb(()) = 31,
+    //// TODO: clunitdb
+    //ClunitDb(()) = 33,
+    //// TODO: `vit_cand`
+    //VitCand(()) = 35,
+    //// TODO: `sts_list`
+    //StsList(()) = 37,
+    //// TODO: userdata
+    //UserData(()) = 41,
+    //// TODO: itemfunc
+    //ItemFunc(()) = 43,
+    //// TODO: features
+    Features(Vec<Feature<'a>>) = 45,
+    //// TODO: breakfunc
+    //BreakFunc(()) = 47,
+    //// TODO: `cg_db`
+    //ClustergenDb(()) = 49,
+    //// TODO: voice
+    //Voice(()) = 51,
+    //// TODO: `audio_streaming_info`
+    //AudioStreamingInfo(()) = 53,
 }
 impl<'a> Value<'a> {
     /// Gets the `Phoneset` value if exists, `None` otherwise
     #[must_use]
-    pub fn phoneset(&self) -> Option<Strong<Phoneset<'a>>> {
+    pub fn phoneset(&'a self) -> Option<&'a Phoneset<'a>> {
         let Value::Phoneset(ph) = self else {
             return None;
         };
-        ph.get()
+        Some(ph)
     }
     /// Gets `str` inner value, `None` otherwise
     #[must_use]
