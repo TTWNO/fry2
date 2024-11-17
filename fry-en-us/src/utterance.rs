639474303ba56de559fe457d63dbc22b909f1039
Tait Hoyem <tait@tait.tech>
2024-11-17 11:15:28 -0800

    Continue work on us_f0_model

--- fry-en-us/src/utterance.rs
+++ fry-en-us/src/utterance.rs
@@ -9,6 +9,15 @@ impl USFZeroModel for Strong<Utterance<'_>> {
         if self.borrow().features.feature_present("no_f0_target_model") {
             return;
         }
+				let targ_rel = self.relation_create("Target");
+				let mean = self.features
+						.feature_value("int_f0_target")
+						.float().unwrap_or(100.0)
+				+
+						self.features
+						.feature_value("f0_shift")
+						.foat().unwrap_or(1.0);
+				let stddev = self.features.feature_value("int_f0_target_stddev").float().unwrap_or(12.0);
         todo!()
     }
 }
